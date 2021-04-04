308dafe9b62c0302bbebfaf1181031f4543ffe79
Vlad Frolov <frolko95@mail.ru>
2021-04-04 20:31:28 +0400

    add methods

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 "use strict";
 
 let num = 266219;
+let num2 = 1;
 
 let arr = (String(num).split(""));
 
@@ -9,6 +10,19 @@ console.log(answer);
 
 console.log(answer **=3);
 
-console.log(typeof(answer));
+console.log(typeof(answer));    
 
 console.log(String(answer).substring(0, 2));
+
+// второй способ
+
+// arr.forEach(function(el) {
+//   return(num2 *= el);
+// });
+// console.log(num2);
+
+// третий способ
+
+var num3 = arr.map( function(el, i) { return num2*=el; } );
+console.log( num3[5] ); 
+
